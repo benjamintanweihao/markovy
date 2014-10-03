6dc8eef15989e0c7ed38bf1be961cb7602e68d9c
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-10-03 19:00:28 +0800

    Initial implementation.

--- lib/markovy.ex
+++ lib/markovy.ex
@@ -1,2 +1,55 @@
 defmodule Markovy do
+  defmodule Dictionary do
+    def new do
+      HashDict.new
+    end
+
+    def build(dict, text) when is_binary(text) do
+      tokens = String.split(text)
+      build(dict, tokens)
+    end
+
+    def build(dict, [prefix_1, prefix_2, suffix|rest]) do
+      prefix   = "#{prefix_1} #{prefix_2}"
+      new_dict = case HashDict.get(dict, prefix) do
+        nil ->
+          HashDict.put_new(dict, prefix, [suffix])
+        suffixes ->
+          HashDict.put_new(dict, prefix, [suffix|suffixes])
+      end
+      build(new_dict, [prefix_2, suffix|rest])
+    end
+
+    def build(dict, _source) do
+      dict
+    end
+  end
+
+  def next(dict, prefix) do
+    HashDict.get(dict, prefix, []) |> Enum.shuffle |> List.first
+  end
+
+  def generate_sentence(dict, seed_prefix, max_length) when is_binary(seed_prefix) do
+    [prefix_1, prefix_2] = String.split(seed_prefix)
+    generate_sentence(dict, [prefix_2, prefix_1], 0, max_length)
+  end
+
+  def generate_sentence(_dict, result, max_length, max_length) do
+    process_result(result)
+  end
+
+  def generate_sentence(dict, [prefix_2, prefix_1|rest] = list, current_length, max_length) do
+    case next(dict, "#{prefix_1} #{prefix_2}") do
+      nil  ->
+        process_result(rest)
+      next ->
+        generate_sentence(dict, [next|list], current_length+1, max_length)
+    end
+  end
+
+  def process_result(result) do
+    result |> Enum.reverse |> Enum.join(" ")
+  end
+
 end
+
--- test/markovy_test.exs
+++ test/markovy_test.exs
@@ -1,7 +1,25 @@
 defmodule MarkovyTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @source "the judge and the jury put the blame on me"
+  @dictionary Markovy.Dictionary.new |> Markovy.Dictionary.build(@source)
+
+  test "generate the next word given a prefix" do
+    dict = Markovy.Dictionary.build(Markovy.Dictionary.new, @source)
+    assert dict |> Markovy.next("the judge")    == "and"
+    assert dict |> Markovy.next("judge and")    == "the"
+    assert dict |> Markovy.next("and the")      == "jury"
+    assert dict |> Markovy.next("the jury")     == "put"
+    assert dict |> Markovy.next("jury put")     == "the"
+    assert dict |> Markovy.next("put the")      == "blame"
+    assert dict |> Markovy.next("the blame")    == "on"
+    assert dict |> Markovy.next("blame on")     == "me"
   end
+
+  test "generates a sentence given seed prefix and a word limit" do
+    dict = Markovy.Dictionary.build(Markovy.Dictionary.new, @source)
+    assert dict |> Markovy.generate_sentence("the judge", 8) == @source
+  end
+
 end
+
